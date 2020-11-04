8f6a5b3d8090c86f590f4e2f03ecf3bec97480b6
Lana <slomteva@outlook.com>
2020-11-04 09:35:25 +0200

    add scroll
    add animation scroll

--- js/script.js
+++ js/script.js
@@ -21,14 +21,23 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
     });
     
-});
 
-/*
-    var waypoint = new Waypoint({
-        element: document.getElementById('px-offset-waypoint'),
-        handler: function(direction) {
-            notify('I am 20px from the top of the window')
-        },
-        offset: 20 
-    })
-*/
+
+/* Navigation scroll */
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+
+});
